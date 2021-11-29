f8f8d9ebe3b0a0859302a028a53a97ed91004d97
karrel <karrel84@naver.com>
2021-11-29 19:51:26 +0900

    module을 app으로 이동

--- app2/src/main/java/me/rell/app/module/RepositoryModule.kt
+++ app2/src/main/java/me/rell/app/module/RepositoryModule.kt
@@ -1,18 +1,18 @@
-package me.rell.data
+package me.rell.app.module
 
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import me.rell.data.ImageRepositoryImpl
 import me.rell.domain.ImageRepository
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-abstract class ImageDataModule {
+abstract class RepositoryModule {
 
     @Singleton
     @Binds
-    abstract fun bindImageRepository(impl:ImageRepositoryImpl): ImageRepository
+    abstract fun bindImageRepository(impl: ImageRepositoryImpl): ImageRepository
 }
